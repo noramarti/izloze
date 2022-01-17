43fc607345c06fa01332acf203e870da67184bf5
noramarti <martinsonenora@gmail.com>
2022-01-17 15:04:05 +0200

    sakums

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1)
+const vardi=['Nora Martinsone','Anna Bērza','Jānis Ozols','Roberts Egle','Linda Bērziņa'];
+const balvas=['Dators','Ledusskapis','Telefons','Automašīna','Viedpulkstenis'];
+const naudaKopa=1000000;//kopējā summa
+let uzvaretajuSkaits=5;
+let rand= Math.random()*10;
+rand= Math.floor(rand);//noapaļo skaitli uz leju
+console.log(vardi[rand]);//izvada konsolē 
