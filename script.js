3847c65af21c0d661f101b3dc39741aa0b4b484e
noramarti <martinsonenora@gmail.com>
2022-01-24 15:04:12 +0200

    2.stunda

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const vardi=['Nora Martinsone','Anna Bērza','Jānis Ozols','Roberts Egle','Linda Bērziņa'];
-const balvas=['Dators','Ledusskapis','Telefons','Automašīna','Viedpulkstenis'];
-const naudaKopa=1000000;//kopējā summa
-let uzvaretajuSkaits=5;
-let rand= Math.random()*10;
-rand= Math.floor(rand);//noapaļo skaitli uz leju
-console.log(vardi[rand]);//izvada konsolē 
+const vardi = ['Nora Martinsone', 'Anna Bērza', 'Jānis Ozols', 'Roberts Egle', 'Linda Bērziņa'];
+const balvas = ['Dators', 'Ledusskapis', 'Telefons', 'Automašīna', 'Viedpulkstenis'];
+const naudaKopa = 100000;//kopējā summa
+let uzvaretajuSkaits = 5;
+let rindas=document.querySelector('.rindas');
+rindas.innerHTML=' ';
+for (let i=0; i < uzvaretajuSkaits; i++) {
+    let rand = Math.random() * vardi.lenght;
+    rand= Math.floor(rand);//noapaļo skaitli uz leju
+    console.log(vardi[rand]);//izvada konsolē 
+}
+rindas.innerHTML=
+<tr>
+    <td>i</td>
+    <td>uzvarētājs</td>
+</tr>
